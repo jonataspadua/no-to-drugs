c87edf3632ee59b9e47d0f4d2d4826d702d68738
Jonatas Sales <Jonatas Sales>
2017-07-09 11:58:58 -0300

    Troquei os scripts para a pasta scripts Necessário para melhor organização

---
+++ No to Drugs/Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	public Material globo;
+
+	void Update () {
+		transform.Rotate (new Vector3 (0, 0, 45) * Time.deltaTime);
+		if (globo.color == Color.blue) {
+			globo.color = Color.red;
+		} else if (globo.color == Color.red) {
+			globo.color = Color.black;
+		} else if (globo.color == Color.blue) {
+			globo.color = Color.blue;
+		}
+	}
+}
